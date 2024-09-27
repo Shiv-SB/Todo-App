7ebd9e64a1f1c4566dee8265a6e7e2be6a6a9816
Shivarjun Bhogal <sbhogal@hcrlaw.com>
2024-09-27 11:49:27 +0100

    Created GET and POST task endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,32 @@
 import { readFile, writeFile } from "node:fs";
 import { serve } from "bun";
+import { Tasks } from "./classes";
+
+import type { Task } from "./types";
 
 const FILE_PATH: string = "./tasks.json";
+
+const tasks = new Tasks(FILE_PATH);
+
+const apiHandler=  async (req: Request) => {
+    const url: URL = new URL(req.url);
+    const path: string = url.pathname;
+    const method: string = req.method;
+
+    // GET /api/tasks
+    if (method === "GET" && path === "/api/tasks") {
+        const allTasks = await tasks.loadAll();
+        return new Response(JSON.stringify(allTasks), { status: 200, headers: { "Content-Type:": "application/json" } });
+    }
+
+    // POST /api/tasks
+    if (method === "POST" && path === "/api/tasks") {
+        const body = await req.json();
+        const allTasks = await tasks.loadAll();
+        const newTask: Task = { id: Date.now(), description: body.description, status: "Open" };
+        allTasks.push(newTask);
+        await tasks.save(allTasks);
+        return new Response(JSON.stringify(newTask), { status: 200, headers: { "Content-Type": "application/json" } });
+    }
+
+}
